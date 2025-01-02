48efd0d1acfa69bdb8a78ae21075548d8e85eed2
102708dreamteam <102708dreamteam@gmail.com>
2025-01-02 10:55:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,10 +58,10 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0004eadf6718241f0" # Change to your preferred AMI
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet1.id
-  security_groups = [aws_security_group.web_sg.name]
+  ami                    = "ami-0004eadf6718241f0" # Change to your preferred AMI
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "WebServer"
@@ -69,18 +69,26 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_lb" "web_lb" {
-  name               = "web-lb"
+  name               = "web-lb-${random_id.lb_id.hex}"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web_sg.id]
   subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 }
 
+resource "random_id" "lb_id" {
+  byte_length = 8
+}
+
 resource "aws_lb_target_group" "web_tg" {
-  name     = "web-tg"
+  name     = "web-tg-${random_id.tg_id.hex}"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+}
+
+resource "random_id" "tg_id" {
+  byte_length = 8
 }
 
 resource "aws_lb_listener" "web_listener" {
@@ -101,7 +109,7 @@ resource "aws_lb_target_group_attachment" "web_tg_attachment" {
 }
 
 resource "aws_iam_role" "ec2_role" {
-  name = "ec2_role"
+  name = "ec2_role_${random_id.role_id.hex}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -115,6 +123,10 @@ resource "aws_iam_role" "ec2_role" {
       },
     ]
   })
+}
+
+resource "random_id" "role_id" {
+  byte_length = 8
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_role_policy" {
