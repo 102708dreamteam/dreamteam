1b40b65793ba44a5d77da48b882c4f13c0a8bca5
102708dreamteam <102708dreamteam@gmail.com>
2025-01-03 09:57:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,32 @@ ami = "ami-01816d07b1128cd2d"
 instance_type = "t2.micro"
 }
 
-resource "aws_s3_bucket" "dreamteam" {
-name = "dream-123"
+resource "aws_iam_role" "web_role" {
+  name = "web_role"
 
+  assume_role_policy = <<-EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }
+    EOF
+}
+
+resource "aws_iam_role_policy_attachment" "web_policy_attachment" {
+  role       = aws_iam_role.web_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "web_profile" {
+  name = "web_profile"
+  role = aws_iam_role.web_role.name
 }
